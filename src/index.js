3dce61bae50e6ab15e6b17d0a619be88c43a50fa
rodrigoaraujosantos <araujo.1983@hotmail.com>
2024-01-17 17:11:45 -0300

    configuração dos headers (cors)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ const rota = require('./routers/rotas');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 app.use(rota);
 
 app.listen(process.env.PORT);
+
